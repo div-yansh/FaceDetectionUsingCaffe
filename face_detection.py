b094c1112de9d8fd11e58e2e68ecd514ec5b4112
div-yansh <46793009+div-yansh@users.noreply.github.com>
2020-02-13 00:36:15 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import cv2 
+
+def detect_face(image):
+	prototxt = "deploy.prototxt.txt"
+	model = "res10_300x300_ssd_iter_140000.caffemodel"
+	confidence_img = 0.5
+
+	net = cv2.dnn.readNetFromCaffe(prototxt, model)
+	
+	image = cv2.imread(image)
+	(h, w) = image.shape[:2]
+	
+	blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, 
+		(300, 300), (104.0, 177.0, 123.0))
+	
+	net.setInput(blob)
+	detections = net.forward()
+	
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0 , i, 2]
+		if confidence > confidence_img:
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY)  = box.astype("int")
+	
+			text = "{:.2f}%".format(confidence * 100)
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.rectangle(image, (startX, startY), (endX, endY),
+				(0, 255, 0), 2)
+			cv2.putText(image, text, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+	
+	cv2.imshow("Output", image)
+	cv2.waitKey(0)
+
+
+if __name__ == '__main__' :
+	image = input()
+	detect_face(image)
